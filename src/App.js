151625d941330edea3f08c0dffc1da3bb1e6014a
nickosp <nickospapageorge85@gmail.com>
2020-10-27 21:18:05 -0400

    made home my default page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Search from  './Search/Search'
 import Home from  './Home/Home'
 import Results from './Results/Results'
 import Nav from './Nav/Nav'
-import { Route } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
       <Nav />
       <main>
         <Route path='/search' component={Search} />
-        <Route path='/home' component={Home} />
+       <Route path='/' exact render={() => <Redirect to='/home' />} />
       </main>
     </div>
     );
